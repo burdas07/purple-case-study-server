c211b7d16e664471d30ec6c3e981756e41701241
Radek Burda <burdas07@gmail.com>
2022-04-20 01:51:42 +0200

    removed createTransaction from POST request

--- source/controllers/transaction.ts
+++ source/controllers/transaction.ts
@@ -2,30 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import Transaction from '../models/transaction';
 
-const createTransaction = (req: Request, res: Response, next: NextFunction) => {
-    let { author, title } = req.body;
-
-    const transaction = new Transaction({
-        _id: new mongoose.Types.ObjectId(),
-        author,
-        title
-    });
-
-    return transaction
-        .save()
-        .then((result) => {
-            return res.status(201).json({
-                book: result
-            });
-        })
-        .catch((error) => {
-            return res.status(500).json({
-                message: error.message,
-                error
-            });
-        });
-};
-
 const getAllTransactions = (req: Request, res: Response, next: NextFunction) => {
     Transaction.find()
         .exec()
@@ -44,4 +20,4 @@ const getAllTransactions = (req: Request, res: Response, next: NextFunction) => 
         });
 };
 
-export default { createTransaction, getAllTransactions };
+export default { getAllTransactions };
